74a24f8cfe309e21ec6e7f11c462048e82e619b5
Jazmín Gil <124318350+JazGil@users.noreply.github.com>
2026-04-23 23:05:45 +0000

    Hero

--- portafolio/app/page.tsx
+++ portafolio/app/page.tsx
@@ -1,10 +1,12 @@
 import Image from "next/image";
 import Header from "./Componentes/Header/Header";
+import Hero from "./Componentes/Hero/Hero";
 
 export default function Home() {
   return (
     <> 
       <Header />
+      <Hero />
     </>
   );
 }
